123e799cc451df0d8b5f93b240b15c451b47a683
MiuMiuMiue <yuanzhe2001@gmail.com>
2023-11-17 18:02:05 -0800

    updating summaryReport.py

--- PreProcess/summaryReport.py
+++ PreProcess/summaryReport.py
@@ -3,7 +3,7 @@ import os
 from tqdm import tqdm
 
 summarizer = pipeline("summarization", model="Mbilal755/Radiology_Bart")
-path = r"D:\MIMIC-CXR\mimic-cxr-jpg_2.0.0\data"
+path = r"D:\UCI_documents\2023_Fall_Quarter\CS 184a\FinalProject\files"
 
 index = 0
 
@@ -11,10 +11,11 @@ for subfolder in os.listdir(path):
     for sub_subfolder in tqdm(os.listdir(os.path.join(path, subfolder)), desc="Processing {}: ".format(index + 1)):
         for sub_sub_subfolder in os.listdir(os.path.join(path, subfolder, sub_subfolder)):
                 path_txt = os.path.join(path, subfolder, sub_subfolder, sub_sub_subfolder, "label")
-                for file in os.listdir(path_txt):
-                    with open(os.path.join(path_txt, file), 'r') as f:
-                        findings = f.read()
-                        summary = summarizer(findings)[0]['summary_text']
-                        with open(os.path.join(path_txt, "summary.txt"), 'w') as f:
-                            f.write(summary)
+                if "summary.txt" not in os.listdir(path_txt):
+                    for file in os.listdir(path_txt):
+                        with open(os.path.join(path_txt, file), 'r') as f:
+                            findings = f.read()
+                            summary = summarizer(findings, max_length=105)[0]['summary_text']
+                            with open(os.path.join(path_txt, "summary.txt"), 'w') as f:
+                                f.write(summary)
     index += 1
